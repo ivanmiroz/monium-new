2020cc435598247d98ff2a18ba212a5ca39b8e5b
ivanmiroz <160476128+ivanmiroz@users.noreply.github.com>
2026-09-24 15:26:37 +0300

    next config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,20 @@
 /** @type {import('next').NextConfig} */
+const repoName = 'имя-вашего-репозитория'; // ← поменяйте
+
 const nextConfig = {
+    output: 'export', // статический экспорт в папку out/
+    basePath: `/${repoName}`, // путь до сайта на github.io
+    assetPrefix: `/${repoName}/`, // префикс для ассетов
+    images: {
+        unoptimized: true, // next/image не работает без сервера
+    },
+    trailingSlash: true, // помогает с роутингом на Pages
     webpack: (config) => {
         config.module.rules.push({
             test: /\.svg$/i,
             issuer: /\.[jt]sx?$/,
             use: ['@svgr/webpack'],
         });
-
         return config;
     },
 };
